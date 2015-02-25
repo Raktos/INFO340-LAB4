5eaeff0e96a1e094e579c9e4bf72d428cda76f8d
Information School <ischoolit@uw.edu>
2015-02-25 12:57:40 -0800

    working prbably

--- Lab4.sql
+++ Lab4.sql
@@ -2,16 +2,15 @@ USE INFO_340_LAB_4;
 
 --1) Copy the entire contents of PRODUCT_VENDOR to a new table that matches your tblUWNetID in the database titled INFO_340_LAB_4.
 
-SELECT *
-INTO tblRAKTOS
-FROM GUITAR_SHOP.dbo.tblPRODUCT_VENDOR;
-
 --Leverage a WHILE loop to update Price under the following conditions:
 
 --If the Product is < avg increase by 10%
 --If Product is > avg do not change
 --Stop updating immediately if the is a product price that exceeds $10,000
---2) Write a query to copy 60 rows of data from Customer_Build.tblCUSTOMER into a new table called tblUWNetID_Cust. Retrieve only rows that have a LastName beginning with 'G' and reside in Texas.
+
+SELECT *
+INTO tblRAKTOS
+FROM GUITAR_SHOP.dbo.tblPRODUCT_VENDOR;
 
 SELECT ProductVendorID
 INTO tblRAKTOS_TMP
@@ -21,32 +20,42 @@ DECLARE @exceeds10k BIT = 0;
 DECLARE @ID INT;
 DECLARE @avg DECIMAL = (SELECT AVG(Price) FROM tblRAKTOS);
 
-WHILE (SELECT COUNT(*) FROM tblRAKTOS) > 0 AND @exceeds10k = 0 BEGIN
+WHILE (SELECT COUNT(*) FROM tblRAKTOS_TMP) > 0 AND @exceeds10k = 0 BEGIN
 	SELECT TOP 1 @ID = ProductVendorID FROM tblRAKTOS_TMP;
 
-	DECLARE @Price INT = (SELECT Price FROM tblRAKTOS WHERE ProductVendorID = @ID);
+	DECLARE @Price DECIMAL = (SELECT Price FROM tblRAKTOS WHERE ProductVendorID = @ID);
 
 	IF @Price > 10000 BEGIN
 		SET @exceeds10k = 1;
 	END
 
 	IF @Price < @avg BEGIN
-		SET @Price = 1.1 * @Price;
-	END
-
-	IF @Price > @avg BEGIN
-		SET @Price = .9 * @Price;
-	END
-
-	UPDATE tblRAKTOS
-	SET Price=@Price
-	WHERE ProductVendorID = @ID;
+		UPDATE tblRAKTOS
+		SET Price = 1.1 * @Price
+		WHERE ProductVendorID = @ID;
+	END	
 
 	DELETE FROM tblRAKTOS_TMP WHERE ProductVendorID = @ID;
 END
 
 DROP TABLE tblRAKTOS_TMP;
+
+--2) Write a query to copy 60 rows of data from Customer_Build.tblCUSTOMER into a new table called tblUWNetID_Cust. Retrieve only rows that have a LastName beginning with 'G' and reside in Texas.
+SELECT TOP 60 * 
+INTO tblRAKTOS_CUST
+FROM CUSTOMER_BUILD.dbo.tblCUSTOMER c
+WHERE c.CustomerLname LIKE 'G%' AND c.CustomerState LIKE '%Texas%';
+
+SELECT * FROM tblRAKTOS_CUST;
+
 --3) Add the column DutyCharge (Numeric 6,2) to the table created in step 1 with a DEFAULT of $6.50
+ALTER TABLE tblRAKTOS
+ADD DutyCharge NUMERIC(6,2) NOT NULL 
+DEFAULT 6.50;
+
+SELECT * FROM tblRAKTOS;
 
 --4) Write a check constraint on your new table that prevents a DutyCharge from being greater than $10 on products with a price greater than $1500.
-
+ALTER TABLE tblRAKTOS_TMP
+ADD CONSTRAINT RAKTOS_DutyChargeLimit
+CHECK ((DutyCharge < 10 AND Price > 1500) OR (Price <= 1500));
